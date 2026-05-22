6152e85a720ac2a28c5c8e9960222cd55d40356e
allpowerfulbob <bob.sheets76@gmail.com>
2026-05-21 21:14:18 -0600

    added exponent and percentage

--- project 2.py
+++ project 2.py
@@ -1,7 +1,7 @@
 while True:
     num1 = float(input("Enter the first number: "))
     num2 = float(input("Enter the second number: "))
-    operator = input("Enter the operator (+, -, *, /) :")
+    operator = input("Enter the operator (+, -, *, /, **, %) :")
     if operator == "+":
         result = num1 + num2
     elif operator == "-":
@@ -13,6 +13,10 @@ while True:
             result = "Error: Cannot divide by zero."
         else:
             result = num1 / num2
+    elif operator == "**":
+        result = num1 ** num2
+    elif operator == "%":
+        result = num1 % num2
     else:
         result = "Invalid operaotor"
     print("The result is: ", result)
