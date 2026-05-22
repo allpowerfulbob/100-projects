7e37da913a393e27bbc259952c57462b67063143
allpowerfulbob <bob.sheets76@gmail.com>
2026-05-21 20:40:57 -0600

    initial commit project 2

---
+++ project 2.py
@@ -0,0 +1,14 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operator = input("Enter the operator (+, -, *, /) :")
+if operator == "+":
+    result = num1 + num2
+elif operator == "-":
+    result = num1 - num2
+elif operator == "*":
+    result = num1 * num2
+elif operator == "/":
+    resutl = num1 / num2
+else:
+    result = "Invalid operaotor"
+print("The result is: ", result)
