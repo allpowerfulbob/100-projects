0ec606a0412f2966cad23231ee712697870c45d2
allpowerfulbob <bob.sheets76@gmail.com>
2026-05-21 21:06:23 -0600

    adding a loop

--- project 2.py
+++ project 2.py
@@ -1,17 +1,22 @@
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
-operator = input("Enter the operator (+, -, *, /) :")
-if operator == "+":
-    result = num1 + num2
-elif operator == "-":
-    result = num1 - num2
-elif operator == "*":
-    result = num1 * num2
-elif operator == "/":
-    if num2 == 0:
-        result = "Error: Cannot divide by zero."
+while True:
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    operator = input("Enter the operator (+, -, *, /) :")
+    if operator == "+":
+        result = num1 + num2
+    elif operator == "-":
+        result = num1 - num2
+    elif operator == "*":
+        result = num1 * num2
+    elif operator == "/":
+        if num2 == 0:
+            result = "Error: Cannot divide by zero."
+        else:
+            result = num1 / num2
     else:
-        result = num1 / num2
-else:
-    result = "Invalid operaotor"
-print("The result is: ", result)
+        result = "Invalid operaotor"
+    print("The result is: ", result)
+
+    again = input("Calclualte again? (y/n): ").lower
+    if again != "y":
+        break
