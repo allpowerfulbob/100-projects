825058a6ebd82a0a8156f37eb4488c5b84b78f33
allpowerfulbob <bob.sheets76@gmail.com>
2026-05-23 08:15:38 -0600

    still troubleshooting

--- Project 3.py
+++ Project 3.py
@@ -1,11 +1,11 @@
-def c_to_f(c):
-    return c * 9/5 + 32
-def f_to_c(f):
-    return (f-32)*5/9
-def c_to_k(c):
-    return c + 273.15
-def k_to_c (k):
-    return k - 273.15
+def c_to_f(C):
+    return C * 9/5 + 32
+def f_to_c(F):
+    return (F-32)*5/9
+def c_to_k(C):
+    return C + 273.15
+def k_to_c (K):
+    return K - 273.15
 def convert():
     while True:
         scale = input("Choose scale (C)elsius, (F)arenheit, "
