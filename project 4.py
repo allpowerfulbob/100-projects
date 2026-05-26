4df6094ba80b907d400402b00b15069920640a30
allpowerfulbob <bob.sheets76@gmail.com>
2026-05-25 21:15:29 -0600

    initial commit

---
+++ project 4.py
@@ -0,0 +1,17 @@
+rates = {
+    ("USD", "EUR"): 0.92,
+    ("USD", "GBP"): 0.80,
+    ("EUR", "USD"): 1.09,
+    ("GBP", "USD"): 1.25,
+    ("EUR", "GBP"): 0.87,
+    ("GBP", "EUR"): 1.15
+    }
+print ("Simple Currency Converter")
+from_curr = input ("From (USD/EUR/GBP): ").upper()
+to_curr = input("To (USD/EUR/GBP): ").upper()
+amount = flota(input("Amount: "))
+if (from_curr, to_curr) in rates:
+    result = amount * rates[(from_curr, to_curr)]
+    print("Converted amount: ", result)
+else:
+    print("Conversion not supported.")
