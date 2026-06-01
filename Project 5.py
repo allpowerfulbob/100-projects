5b566e687c0ddd8c6af14415106c87765362fd69
allpowerfulbob <bob.sheets76@gmail.com>
2026-05-31 20:11:48 -0600

    corrected spelling

--- Project 5.py
+++ Project 5.py
@@ -8,10 +8,9 @@ while guess != number_to_guess:
     except ValueError:
         print("Please enter a valid number.")
     continue
-print("You made it to low number.")
 if guess < number_to_guess:
     print("Too low! Try again.")
 elif guess > number_to_guess:
     print("Too high! Try again.")
 else:
-    print(f"Congratulations! Youguessed the number:{number_to_guess}")
+    print(f"Congratulations! You guessed the number: {number_to_guess}")
