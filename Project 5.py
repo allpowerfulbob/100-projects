81527d3a15e85effdb638642516c0c9110b25d01
allpowerfulbob <bob.sheets76@gmail.com>
2026-05-31 20:00:26 -0600

    working on it

--- Project 5.py
+++ Project 5.py
@@ -9,9 +9,9 @@ while guess != number_to_guess:
         print("Please enter a valid number.")
     continue
     print("You made it here.")
-if guess < number_to_guess:
-    print("Too low! Try again.")
-elif guess > number_to_guess:
-    print("Too high! Try again.")
-else:
-    print(f"Congratulations! Youguessed the number:{number_to_guess}")
+    if guess < number_to_guess:
+        print("Too low! Try again.")
+    elif guess > number_to_guess:
+        print("Too high! Try again.")
+    else:
+        print(f"Congratulations! Youguessed the number:{number_to_guess}")
