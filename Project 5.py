2d6bbd952a566d61a922a9301e616fe3dee189cc
allpowerfulbob <bob.sheets76@gmail.com>
2026-05-31 19:48:03 -0600

    initial commit

---
+++ Project 5.py
@@ -0,0 +1,16 @@
+import random
+print("Welcome to the number guessing game")
+number_to_guess = random.ranint(1,100)
+guess = None
+while guess !=number_to_guess:
+    try:
+        guess = int(input("Enter your guess(1-100):"))
+    except ValueError:
+        print("Please enter a valid number.")
+    continue
+if guess < number_to_guess:
+    print("Too low! Try again.")
+elif guess > number_to_guess:
+    print("Too high! Try again.")
+else:
+    print(f"Congratulations! Youguessed the number:{number_to_guess}")
