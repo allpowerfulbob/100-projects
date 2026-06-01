5127b06ff96af598f4d199ed1821a1ee62780b9d
allpowerfulbob <bob.sheets76@gmail.com>
2026-05-31 20:09:59 -0600

    adding more troubleshooting

--- Project 5.py
+++ Project 5.py
@@ -8,7 +8,7 @@ while guess != number_to_guess:
     except ValueError:
         print("Please enter a valid number.")
     continue
-print("You made it here.")
+print("You made it to low number.")
 if guess < number_to_guess:
     print("Too low! Try again.")
 elif guess > number_to_guess:
