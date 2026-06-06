8f5276913ead3eb10877f31fea2fddd11d0a8d8f
allpowerfulbob <bob.sheets76@gmail.com>
2026-06-06 09:14:11 -0600

    added attempts counter

--- Project 5.py
+++ Project 5.py
@@ -1,4 +1,5 @@
 import random
+attempts = 0
 print("Welcome to the number guessing game")
 number_to_guess = random.randint(1,10)
 guess = None
@@ -8,9 +9,10 @@ while guess != number_to_guess:
     except ValueError:
         print("Please enter a valid number.")
     continue
+attempts += 1
 if guess < number_to_guess:
     print("Too low! Try again.")
 elif guess > number_to_guess:
     print("Too high! Try again.")
 else:
-    print(f"Congratulations! You guessed the number: {number_to_guess}")
+    print(f"Congratulations! You guessed the number: {number_to_guess} in {attempts} attempts.")
